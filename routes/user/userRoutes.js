5183e09d18258ea248c5ba1765c953602f3d88af
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-17 19:11:28 +0600

    update register validation

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
+const registerValidations = [
+  body("name")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Name Is Required!!"),
+
+  body("email")
+    .isEmail()
+    .trim()
+    .normalizeEmail()
+    .withMessage("Email Is Required!!"),
+
+  body("password")
+    .isLength({ min: 8 })
+    .trim()
+    .withMessage("Password Should be 8 Character long!"),
+];
+
+router.post("/register", registerValidations, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 });
 
 module.exports = router;

feec18931a85b8058be89c9f79fde89a29c60718
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-17 17:48:21 +0600

    User Router

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const env = require("./config/envConfig");
 const dbConnect = require("./config/db");
+const userRouters = require("./routes/user/userRoutes");
 
 app = express();
 
 // DB Connection
 dbConnect();
 
+// User Router
+app.use("/api/auth", userRouters);
+
+// Test route
 app.get("/", (req, res) => {
   res.json({ msg: "welcome to gRio." });
 });
 
+// PORT selection
 const port = env.PORT || 8081;
 
+// Check PORT
 app.listen(port, () => {
   console.log(`gRio Running on port ${port}`);
 });
---
+++ routes/user/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+});
+
+module.exports = router;

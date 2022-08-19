8892433b16d2468c8de4f37389cf6346a3171847
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-08-19 15:46:58 +0600

    Private Route setup

---
+++ client/src/routes/Private.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-router-dom";
+
+const Private = ({ children }) => {
+  return <div></div>;
+};
+
+export default Private;

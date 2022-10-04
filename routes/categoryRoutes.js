4044fbe0d73be06eb24eb2b24b2c67eea0fac5b7
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-04 09:10:18 +0600

    Using Middleware for authorization

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,6 +4,10 @@ const { categoryValidtion } = require("../validations/categoryValidation");
 const Authorization = require("./../services/Authorization");
 const router = express.Router();
 
-router.post("/create-category", categoryValidtion, Category.createCategory);
+router.post(
+  "/create-category",
+  [categoryValidtion, Authorization.authorized],
+  Category.createCategory
+);
 
 module.exports = router;

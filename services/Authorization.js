2422eac88d779bd7a5111ed9fa55f93b8ea2d397
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-04 09:35:26 +0600

    Create Category Route Protected In Server Site

--- services/Authorization.js
+++ services/Authorization.js
@@ -1,16 +1,22 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SEC } = require("./../config/envConfig");
+const { JWT_CODE } = require("./../config/envConfig");
 
 class Authorization {
   authorized(req, res, next) {
     const headerToken = req.headers.authorization;
     if (headerToken) {
       const token = headerToken.split("Bearer ")[1];
-      console.log(token);
+      const verify = jwt.verify(token, JWT_CODE);
+
+      if (verify) {
+        next();
+      } else {
+        return res.status(401).json({ errors: [{ msg: "Need Valid Token." }] });
+      }
     } else {
       return res
         .status(401)
-        .json({ errors: [{ msg: "UnAuthorized access!!" }] });
+        .json({ errors: [{ msg: "Need Token For access!!" }] });
     }
   }
 }

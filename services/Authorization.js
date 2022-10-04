a3a0602b006d600c99394a5d1e749ee45fc0bd69
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-04 09:16:02 +0600

    If token not valid

--- services/Authorization.js
+++ services/Authorization.js
@@ -1,5 +1,13 @@
 class Authorization {
-  authorized(req, res, next) {}
+  authorized(req, res, next) {
+    const headerToken = req.headers.authorization;
+    if (headerToken) {
+    } else {
+      return res
+        .status(401)
+        .json({ errors: [{ msg: "UnAuthorized access!!" }] });
+    }
+  }
 }
 
 module.exports = new Authorization();

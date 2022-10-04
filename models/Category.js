0f3e8b2bb66f954c2f4d7d10d4c2dc9ec789d58e
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-04 11:24:12 +0600

    Category Model Update

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true }, {timestamps:true}
 });
 
 module.exports.CategoryModel = mongoose.model("Category", categorySchema);

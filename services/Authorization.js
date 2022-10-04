778663cb98aaafdd36c04855478f94c58c4193e4
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-04 09:21:27 +0600

    Split Token

--- services/Authorization.js
+++ services/Authorization.js
@@ -1,7 +1,10 @@
+const jwt = require("jsonwebtoken");
 class Authorization {
   authorized(req, res, next) {
     const headerToken = req.headers.authorization;
     if (headerToken) {
+      const token = headerToken.split("Bearer ")[1];
+      console.log(token);
     } else {
       return res
         .status(401)

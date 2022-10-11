6849d49ac9f4beaabb0b952dcd38a3116ad2a5e8
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 11:57:25 +0600

    create product page test

--- client/src/screens/dashboard/CreateProduct.jsx
+++ client/src/screens/dashboard/CreateProduct.jsx
@@ -14,6 +14,7 @@ import "react-quill/dist/quill.snow.css";
 import { useCProductMutation } from "../../store/services/productServices";
 import { useDispatch } from "react-redux";
 import { setSuccess } from "../../store/reducers/globalReducer";
+import toast, { Toaster } from "react-hot-toast";
 
 const CreateProduct = () => {
   const { data = [], isFetching } = useAllCategoriesQuery();
@@ -97,21 +98,23 @@ const CreateProduct = () => {
     createNewProduct(formData);
   };
 
-  // useEffect(() => {
-  //   if (!response.isSuccess) {
-  //     response?.error?.data?.errors.map((err) => {
-  //       toast.error(err.msg);
-  //     });
-  //   }
-  // }, [response?.error?.data?.errors]);
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   if (response?.isSuccess) {
-  //     dispatch(setSuccess(response?.data?.msg));
-  //     navigate("/dashboard/products");
-  //   }
-  // }, [response?.isSuccess]);
+  useEffect(() => {
+    if (!response.isSuccess) {
+      response?.error?.data?.errors.map((err) => {
+        toast.error(err.msg);
+      });
+    }
+  }, [response?.error?.data?.errors]);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (response?.isSuccess) {
+      dispatch(setSuccess(response?.data?.msg));
+      navigate("/dashboard/products");
+    }
+  }, [response?.isSuccess]);
 
   return (
     <div>
@@ -120,186 +123,187 @@ const CreateProduct = () => {
           <Link to="/dashboard/products" className="btn-dark">
             <i className="bi bi-arrow-left-short"></i> proudcts list
           </Link>
-          <div className="flex flex-wrap -mx-3">
-            <form className="w-full xl:w-8/12 p-3" onSubmit={createPro}>
-              <div className="flex flex-wrap">
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="title" className="label">
-                    title
-                  </label>
-                  <input
-                    type="text"
-                    name="title"
-                    className="form-control"
-                    id="title"
-                    placeholder="title..."
-                  />
-                </div>
-
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="price" className="label">
-                    price
-                  </label>
-                  <input
-                    type="number"
-                    name="price"
-                    className="form-control"
-                    id="price"
-                    placeholder="price..."
-                    onChange={handleInput}
-                    value={state.price}
-                  />
-                </div>
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="discount" className="label">
-                    discount
-                  </label>
-                  <input
-                    type="number"
-                    name="discount"
-                    className="form-control"
-                    id="discount"
-                    placeholder="discount..."
-                    onChange={handleInput}
-                    value={state.discount}
-                  />
-                </div>
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="stock" className="label">
-                    stock
-                  </label>
-                  <input
-                    type="number"
-                    name="stock"
-                    className="form-control"
-                    id="stock"
-                    placeholder="stock..."
-                    onChange={handleInput}
-                    value={state.stock}
-                  />
-                </div>
-
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="categories" className="label">
-                    categories
-                  </label>
-                  {!isFetching ? (
-                    data?.categories?.length > 0 && (
-                      <select
-                        name="category"
-                        id="categories"
-                        className="form-control"
-                        onChange={handleInput}
-                        value={state.category}
-                      >
-                        <option value="">Choose category</option>
-                        {data?.categories?.map((category) => (
-                          <option value={category.name} key={category._id}>
-                            {category.name}
-                          </option>
-                        ))}
-                      </select>
-                    )
-                  ) : (
-                    <Spinner />
-                  )}
-                </div>
-                <div className="w-full md:w-6/12 p-3">
-                  <label htmlFor="colors" className="label">
-                    choose colors
-                  </label>
-                  <TwitterPicker onChangeComplete={saveColors} />
-                </div>
-
-                <div className="w-full p-3">
-                  <label htmlFor="sizes" className="label">
-                    choose sizes
-                  </label>
-                  {sizes.length > 0 && (
-                    <div className="flex flex-wrap -mx-3">
-                      {sizes.map((size) => (
-                        <div
-                          key={size.name}
-                          className="size"
-                          onClick={() => chooseSize(size)}
-                        >
-                          {size.name}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-
-                <div className="w-full p-3">
-                  <label htmlFor="image1" className="label">
-                    Image 1
-                  </label>
-                  <input
-                    type="file"
-                    name="image1"
-                    id="image1"
-                    className="input-file"
-                    onChange={imageHandle}
-                  />
-                </div>
-
-                <div className="w-full p-3">
-                  <label htmlFor="image2" className="label">
-                    Image 2
-                  </label>
-                  <input
-                    type="file"
-                    name="image2"
-                    id="image2"
-                    className="input-file"
-                    onChange={imageHandle}
-                  />
-                </div>
-
-                <div className="w-full p-3">
-                  <label htmlFor="image3" className="label">
-                    Image 3
-                  </label>
-                  <input
-                    type="file"
-                    name="image3"
-                    id="image3"
-                    className="input-file"
-                    onChange={imageHandle}
-                  />
-                </div>
-                <div className="w-full p-3">
-                  <label htmlFor="description" className="label">
-                    Description
-                  </label>
-                  <ReactQuill
-                    theme="snow"
-                    id="description"
-                    value={value}
-                    onChange={setValue}
-                    placeholder="Description..."
-                  />
-                </div>
-                <div className="w-full p-3">
-                  <input
-                    type="submit"
-                    value={response.isLoading ? "loading..." : "save product"}
-                    disabled={response.isLoading ? true : false}
-                    className="btn btn-submit"
-                  />
-                </div>
-              </div>
-            </form>
-
-            <div className="w-full xl:w-4/12 p-3">
-              <Colors colors={state.colors} deleteColor={deleteColor} />
-              <SizesList list={sizeList} deleteSize={deleteSize} />
-              <ImagesPreview url={preview.image1} heading="image 1" />
-              <ImagesPreview url={preview.image2} heading="image 2" />
-              <ImagesPreview url={preview.image3} heading="image 3" />
-            </div>
-          </div>
         </ScreenHeader>
+        <Toaster position="top-right" reverseOrder={true} />
+        <div className="flex flex-wrap -mx-3">
+          <form className="w-full xl:w-8/12 p-3" onSubmit={createPro}>
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="title" className="label">
+                  title
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  className="form-control"
+                  id="title"
+                  placeholder="title..."
+                />
+              </div>
+
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="price" className="label">
+                  price
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  className="form-control"
+                  id="price"
+                  placeholder="price..."
+                  onChange={handleInput}
+                  value={state.price}
+                />
+              </div>
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="discount" className="label">
+                  discount
+                </label>
+                <input
+                  type="number"
+                  name="discount"
+                  className="form-control"
+                  id="discount"
+                  placeholder="discount..."
+                  onChange={handleInput}
+                  value={state.discount}
+                />
+              </div>
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="stock" className="label">
+                  stock
+                </label>
+                <input
+                  type="number"
+                  name="stock"
+                  className="form-control"
+                  id="stock"
+                  placeholder="stock..."
+                  onChange={handleInput}
+                  value={state.stock}
+                />
+              </div>
+
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="categories" className="label">
+                  categories
+                </label>
+                {!isFetching ? (
+                  data?.categories?.length > 0 && (
+                    <select
+                      name="category"
+                      id="categories"
+                      className="form-control"
+                      onChange={handleInput}
+                      value={state.category}
+                    >
+                      <option value="">Choose category</option>
+                      {data?.categories?.map((category) => (
+                        <option value={category.name} key={category._id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  )
+                ) : (
+                  <Spinner />
+                )}
+              </div>
+              <div className="w-full md:w-6/12 p-3">
+                <label htmlFor="colors" className="label">
+                  choose colors
+                </label>
+                <TwitterPicker onChangeComplete={saveColors} />
+              </div>
+
+              <div className="w-full p-3">
+                <label htmlFor="sizes" className="label">
+                  choose sizes
+                </label>
+                {sizes.length > 0 && (
+                  <div className="flex flex-wrap -mx-3">
+                    {sizes.map((size) => (
+                      <div
+                        key={size.name}
+                        className="size"
+                        onClick={() => chooseSize(size)}
+                      >
+                        {size.name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+
+              <div className="w-full p-3">
+                <label htmlFor="image1" className="label">
+                  Image 1
+                </label>
+                <input
+                  type="file"
+                  name="image1"
+                  id="image1"
+                  className="input-file"
+                  onChange={imageHandle}
+                />
+              </div>
+
+              <div className="w-full p-3">
+                <label htmlFor="image2" className="label">
+                  Image 2
+                </label>
+                <input
+                  type="file"
+                  name="image2"
+                  id="image2"
+                  className="input-file"
+                  onChange={imageHandle}
+                />
+              </div>
+
+              <div className="w-full p-3">
+                <label htmlFor="image3" className="label">
+                  Image 3
+                </label>
+                <input
+                  type="file"
+                  name="image3"
+                  id="image3"
+                  className="input-file"
+                  onChange={imageHandle}
+                />
+              </div>
+              <div className="w-full p-3">
+                <label htmlFor="description" className="label">
+                  Description
+                </label>
+                <ReactQuill
+                  theme="snow"
+                  id="description"
+                  value={value}
+                  onChange={setValue}
+                  placeholder="Description..."
+                />
+              </div>
+              <div className="w-full p-3">
+                <input
+                  type="submit"
+                  value={response.isLoading ? "loading..." : "save product"}
+                  disabled={response.isLoading ? true : false}
+                  className="btn btn-submit"
+                />
+              </div>
+            </div>
+          </form>
+
+          <div className="w-full xl:w-4/12 p-3">
+            <Colors colors={state.colors} deleteColor={deleteColor} />
+            <SizesList list={sizeList} deleteSize={deleteSize} />
+            <ImagesPreview url={preview.image1} heading="image 1" />
+            <ImagesPreview url={preview.image2} heading="image 2" />
+            <ImagesPreview url={preview.image3} heading="image 3" />
+          </div>
+        </div>
       </Wrapper>
     </div>
   );
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const Authorization = require("../services/Authorization");
+const productValidations = require("../validations/productValidations");
 const router = new express.Router();
 const Product = require("./../controllers/product");
 
-router.post("/create-product", Authorization.authorized, Product.create);
+router.post(
+  "/create-product",
+  [Authorization.authorized, productValidations],
+  Product.create
+);
 
 module.exports = router;

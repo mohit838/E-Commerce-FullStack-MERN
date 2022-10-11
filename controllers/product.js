5401679fb134191d38eabb0c3d3ae0589b348643
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 12:49:14 +0600

    Update Server site product

--- controllers/product.js
+++ controllers/product.js
@@ -11,6 +11,17 @@ class Product {
     form.parse(req, async (err, fields, files) => {
       if (!err) {
         const parsedData = JSON.parse(fields.data);
+
+        req.body.title = parsedData.title;
+        req.body.price = parsedData.price;
+        req.body.discount = parsedData.discount;
+        req.body.stock = parsedData.stock;
+        req.body.category = parsedData.category;
+        req.body.description = parsedData.description;
+
+        const errors = validationResult(res);
+
+        console.log(req.body);
       }
     });
   }

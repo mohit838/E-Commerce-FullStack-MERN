0e7f16355e9a7ee93896b6ffd77b6afdc17901dc
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 10:33:33 +0600

    Create Products Page update

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -3,11 +3,13 @@ import authReducer from "./reducers/authReducer";
 import globalReducer from "./reducers/globalReducer";
 import authService from "./services/authService";
 import createServices from "./services/categoryServices";
+import productService from "./services/productServices";
 
 export const Store = configureStore({
   reducer: {
     [authService.reducerPath]: authService.reducer,
     [createServices.reducerPath]: createServices.reducer,
+    [productService.reducerPath]: productService.reducer,
     authReducer: authReducer,
     globalReducer: globalReducer,
   },

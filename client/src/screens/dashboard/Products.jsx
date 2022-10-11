33367ce7ebdd754930ffeeff005f12a623c5e353
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 08:47:23 +0600

    Main Product page Updae

--- client/src/screens/dashboard/Products.jsx
+++ client/src/screens/dashboard/Products.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 import ScreenHeader from "../../components/ScreenHeader";
 import Wrapper from "./Wrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { clearMessage } from "../../store/reducers/globalReducer";
 
 const Products = () => {
+  const { success } = useSelector((state) => state.globalReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (success) {
+      toast.success(success);
+    }
+    return () => {
+      dispatch(clearMessage());
+    };
+  }, []);
+
   return (
     <>
       <Wrapper>
@@ -11,6 +25,7 @@ const Products = () => {
           <Link to="/dashboard/create-product" className="btn-dark">
             create product
           </Link>
+          <Toaster position="top-right" />
         </ScreenHeader>
       </Wrapper>
     </>

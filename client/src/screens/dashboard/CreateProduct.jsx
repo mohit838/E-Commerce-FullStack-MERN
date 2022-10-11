1f3d6a97f64df2b288f159c93c29fd3f94e1bb71
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 09:45:53 +0600

    Insert Color

--- client/src/screens/dashboard/CreateProduct.jsx
+++ client/src/screens/dashboard/CreateProduct.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import toast, { Toaster } from "react-hot-toast";
 import Wrapper from "./Wrapper";
 import ScreenHeader from "../../components/ScreenHeader";
 import { useAllCategoriesQuery } from "../../store/services/categoryServices";
 import Spinner from "../../components/Spinner";
+import { TwitterPicker } from "react-color";
 
 const CreateProduct = () => {
   const { data = [], isFetching } = useAllCategoriesQuery();
@@ -14,10 +14,19 @@ const CreateProduct = () => {
     discount: 0,
     stock: 0,
     category: "",
+    colors: [],
   });
 
   const handleInput = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const saveColors = (color) => {
+    const filtered = state.colors.filter((clr) => clr.color !== color.hex);
+    setState({
+      ...state,
+      colors: [...filtered, { color: color.hex, id: uuidv4() }],
+    });
   };
 
   return (
@@ -27,7 +36,6 @@ const CreateProduct = () => {
           <Link to="/dashboard/products" className="btn-dark">
             <i className="bi bi-arrow-left-short"></i> proudcts list
           </Link>
-          {/* <Toaster position="top-right" reverseOrder={true} /> */}
           <div className="flex flex-wrap -mx-3">
             <form className="w-full xl:w-8/12 p-3">
               <div className="flex flex-wrap">
@@ -111,6 +119,12 @@ const CreateProduct = () => {
                   ) : (
                     <Spinner />
                   )}
+                </div>
+                <div className="w-full md:w-6/12 p-3">
+                  <label htmlFor="colors" className="label">
+                    choose colors
+                  </label>
+                  <TwitterPicker onChangeComplete={saveColors} />
                 </div>
               </div>
             </form>

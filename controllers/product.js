e7ebdfab45939d97b7a7c41a43b1abff9d0913a5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-11 15:51:46 +0600

    Clear Errors

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,5 @@
 const formidable = require("formidable");
 // const { v4: uuidv4 } = require("uuid");
-const { validationResult } = require("express-validator");
 // const fs = require("fs");
 // const path = require("path");
 // const ProductModel = require("./../models/ProductModel");
@@ -8,27 +7,7 @@ const { validationResult } = require("express-validator");
 class Product {
   async create(req, res) {
     const form = formidable({ multiples: true });
-    form.parse(req, async (err, fields, files) => {
-      if (!err) {
-        const parsedData = JSON.parse(fields.data);
-
-        req.body.title = parsedData.title;
-        req.body.price = parsedData.price;
-        req.body.discount = parsedData.discount;
-        req.body.stock = parsedData.stock;
-        req.body.category = parsedData.category;
-        req.body.description = fields.description;
-
-        const errors = validationResult(req);
-
-        if (!errors) {
-          console.log("No Error!");
-        } else {
-          console.log(errors.array());
-          return res.status(400).json({ errors: errors.array() });
-        }
-      }
-    });
+    form.parse(req, async (err, fields, files) => {});
   }
 }
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const Authorization = require("../services/Authorization");
-const productValidations = require("../validations/productValidations");
 const router = new express.Router();
 const Product = require("./../controllers/product");
 
-router.post(
-  "/create-product",
-  [Authorization.authorized, productValidations],
-  Product.create
-);
+router.post("/create-product", [Authorization.authorized], Product.create);
 
 module.exports = router;

d9102fb1968410a29c400c2a3a6e99815e3f7dfb
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-25 19:10:40 +0600

    Update HomeProducts Routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ const Authorization = require("../services/Authorization");
 const productValidations = require("../validations/productValidations");
 const router = new express.Router();
 const Product = require("../controllers/Product");
+const HomeProducts = require("../controllers/HomeProducts");
 
 // Create new products
 router.post("/create-product", [Authorization.authorized], Product.create);
@@ -21,5 +22,11 @@ router.put(
 
 // Delete
 router.delete("/delete/:id", Authorization.authorized, Product.deleteProduct);
+
+// CatProducts or HomeProducs page
+router.get("/cat-products/:name/:page?", HomeProducts.catProducts);
+
+// Search Products Pages For HomeProducts page
+router.get("/search-products/:keyword/:page?", HomeProducts.catProducts);
 
 module.exports = router;

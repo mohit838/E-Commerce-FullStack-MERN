24b73e98ca934c6281ccc45b9019690fb25936fd
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-25 19:30:31 +0600

    Update Home Page

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import Categories from "../../components/home/Categories";
 import Nav from "../../components/home/Nav";
 import Slider from "../../components/home/Slider";
+import { useRandomCategoriesQuery } from "../../store/services/categoryServices";
+import HomeProduct from "./../../components/home/HomeProduct";
 
 const Home = () => {
+  const { data, isFetching } = useRandomCategoriesQuery();
+
+  console.log(data);
+
   return (
     <>
       <Nav />
@@ -12,6 +18,11 @@ const Home = () => {
       </div>
       <div className="my-container mt-10">
         <Categories />
+        {!isFetching &&
+          data?.categories?.length > 0 &&
+          data?.categories.map((category) => (
+            <HomeProduct category={category} key={category._id} />
+          ))}
       </div>
     </>
   );

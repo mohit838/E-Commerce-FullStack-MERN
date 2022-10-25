47ca5dfe5ae76ddaf0184f95f72a8646aafa8466
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-25 19:13:38 +0600

    Home products redux setups

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -3,6 +3,7 @@ import authReducer from "./reducers/authReducer";
 import globalReducer from "./reducers/globalReducer";
 import authService from "./services/authService";
 import createServices from "./services/categoryServices";
+import homeProducts from "./services/homeProducts";
 import productService from "./services/productServices";
 
 export const Store = configureStore({
@@ -10,6 +11,7 @@ export const Store = configureStore({
     [authService.reducerPath]: authService.reducer,
     [createServices.reducerPath]: createServices.reducer,
     [productService.reducerPath]: productService.reducer,
+    [homeProducts.reducerPath]: homeProducts.reducer,
     authReducer: authReducer,
     globalReducer: globalReducer,
   },
@@ -17,6 +19,7 @@ export const Store = configureStore({
     getDefaultMiddleware().concat([
       createServices.middleware,
       productService.middleware,
+      homeProducts.middleware,
     ]),
 });
 

21091733c5579d472e9749dc94acf691e422b816
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-29 17:06:06 +0600

    update Product

--- controllers/product.js
+++ controllers/product.js
@@ -137,10 +137,11 @@ class Product {
     const { id } = req.params;
     try {
       const product = await ProductModel.findOne({ _id: id });
+
+      console.log(product);
       return res.status(200).json(product);
     } catch (error) {
       return res.status(500).json({ error: error.message });
-      console.log(error.message);
     }
   }
 

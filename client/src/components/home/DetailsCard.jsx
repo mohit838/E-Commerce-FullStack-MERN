e6bd530f776c94e0e419ae7dccad062cbeb58899
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-26 18:26:09 +0600

    Products Details

--- client/src/components/home/DetailsCard.jsx
+++ client/src/components/home/DetailsCard.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import DetailsImage from "./DetailsImage";
 
-const DetailsCard = () => {
-  return <div>DetailsCard</div>;
+const DetailsCard = ({ product }) => {
+  return (
+    <>
+      <div className="flex flex-wrap -mx-5">
+        <div className="w-full order-2 md:order-1 md:w-6/12 p-5">
+          <div className="flex flex-wrap -mx-1">
+            <DetailsImage image={product.image1} />
+            <DetailsImage image={product.image2} />
+            <DetailsImage image={product.image3} />
+          </div>
+        </div>
+        <div className="w-full order-1 md:order-2 md:w-6/12 p-5">
+          <h1 className="text-2xl font-bold text-gray-900 capitalize">
+            {product.title}
+          </h1>
+          <div className="flex justify-between my-5">
+            <span className="text-2xl font-bold text-gray-900"> Discount</span>
+            <span className="text-xl line-through text-gray-500">
+              Product Price
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DetailsCard;
